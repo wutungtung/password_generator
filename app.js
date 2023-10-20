64e170c85d5ceebc7fd559c4058f7ab895e92591
wutungtung <112239752+wutungtung@users.noreply.github.com>
2023-10-21 00:24:53 +0800

    1st commit
    app.js:21:15

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const exphbs = require("express-handlebars");
+const generatePassword = require("./generate_password");
 const app = express();
 const port = 3000;
 
@@ -8,7 +10,16 @@ app.set("view engine", "handlebars");
 
 // app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  console.log("req.body", req.body);
+  console.log(generatePassword());
+
   res.render("index");
 });
 
